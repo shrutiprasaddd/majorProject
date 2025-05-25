7b163d00ca793ac2b37ca2ba63b1dac16f05210c
Shruti Prasad <shruti141203@gmail.com>
2025-05-25 12:00:21 +0530

    Final app ready

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ const store = MongoStore.create({
    touchAfter: 24*3600,
 });
 
-store.on("error", () => {
+store.on("error", (err) => {
    console.log("ERROR in MONGO SESSION STORE", err);
 });
 const sessionOptions = {
