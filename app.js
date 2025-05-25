2933f3080bc6281c64ae1295fece3444f78e5909
Shruti Prasad <shruti141203@gmail.com>
2025-05-25 11:58:12 +0530

    Final app ready

--- app.js
+++ app.js
@@ -107,6 +107,7 @@ app.use((err, req, res, next) => {
    // res.status(statusCode).send(message);
 });
 
- app.listen(8080, () => {
-    console.log("server is listening to port 8080");
- });
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`server is listening to port ${PORT}`);
+});
